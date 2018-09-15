fb0543ed01f014de948c3fa0383844c930e47ef0
jdrichardstech <jdrichardstech@gmail.com>
2018-09-15 15:26:34 -0400

    add currying examples and snapshot testing definition

--- JS/currying.js
+++ JS/currying.js
@@ -6,15 +6,38 @@ A function that accepts a single argument is unary, two arguments binary,
 three arguments ternary, and n arguments is n-ary. Therefore, we can define currying
  as the process of taking an n-ary function and turning it into n unary functions. 
  Let's start with a simple example, a function that takes the dot product of two vectors. 
- Recall from linear algebra that the dot product of two vectors 
- [a, b, c] and [x, y, z] is equal to ax + by + cz.
 
 
- function dot(vector1, vector2) {
-  return vector1.reduce((sum, element, index) => sum += element * vector2[index], 0);
-}
+ https://code.tutsplus.com/tutorials/a-gentle-introduction-to-higher-order-components-in-react--cms-30094
+ Currying in Functional Programming
+Although the name suggests that it has something to do with an exotic dish from the popular Indian cuisine, it doesn't. Currying helps you break down a function that takes many arguments into a series of functions that take one argument at a time. Here is an example:
 
-const v1 = [1, 3, -5];
-const v2 = [4, -2, -1];
+//Usual sum function
+const sum = (a, b) => a + b
+ 
+//Curried sum function 
+const curriedSum = function (a) {
+    return function (b) {
+        return a+b
+    }
+ 
+//Curried sum function using arrow syntax
+const curriedSum = a => b => a+b
+ 
+curriedSum(5)(4)
+//9
 
-console.log(dot(v1, v2)); // 1(4) + 3(-2) + (-5)(-1) = 4 - 6 + 5 = 3
+The function accepts just one argument and returns a function that 
+takes in another argument, and this continues until all the arguments are 
+satisfied. 
+
+curriedSum
+// (a) => (b) => a+b
+ 
+curriedSum(4)
+ 
+// (b) => 4+b
+ 
+curriedSum(4)(5)
+ 
+//4+5
